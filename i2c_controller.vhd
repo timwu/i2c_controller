d361c5cc0750a184ac18dcc40c2a1f4efc521a02
Tim Wu <tim.wu.0@gmail.com>
2015-03-05 19:56:51 -0800

    break into procedures

--- i2c_controller.vhd
+++ i2c_controller.vhd
@@ -35,17 +35,19 @@ architecture RTL of i2c_controller is
 	constant high_ticks       : natural := t_pulse_high / clock_period;
 	constant data_setup_ticks : natural := t_data_setup / clock_period;
 
-	type ControllerState_t is (Idle, Start, WriteData, ReadData, WriteDone, ReadDone, Stop);
+	type ControllerState_t is (Idle, Start, SendDeviceAddr, SendDeviceAddrDone, WriteData, ReadData, WriteDone, ReadDone, Stop);
 	type OutputState_t is (Low, High, Released);
+	type AckState_t is (Waiting, Ok, Error);
 
 	signal sclState : OutputState_t := Released;
 	signal sdaState : OutputState_t := Released;
 
-	signal controllerState   : ControllerState_t := Idle;
-	signal sendingDeviceAddr : std_logic;
+	signal controllerState : ControllerState_t := Idle;
 
 	signal shiftRegister : std_logic_vector(7 downto 0);
 	signal shiftCount    : integer range 0 to 8;
+
+	signal ackState : AckState_t := Waiting;
 begin
 	with sclState select SCL <=
 		'0' when Low,
@@ -61,7 +63,10 @@ begin
 		'0' when Idle,
 		'1' when others;
 
-	done <= '1' when controllerState = WriteDone and sendingDeviceAddr = '0' else '1' when controllerState = ReadDone else '0';
+	with controllerState select done <=
+		'1' when ReadDone,
+		'1' when WriteDone,
+		'0' when others;
 
 	with controllerState select currentState <=
 		x"00" when Idle,
@@ -71,10 +76,201 @@ begin
 		x"04" when WriteDone,
 		X"05" when ReadDone,
 		x"06" when Stop,
+		x"07" when SendDeviceAddr,
+		x"08" when SendDeviceAddrDone,
 		x"FF" when others;
 
 	process(clk, rst)
 		variable counter : integer range 0 to low_ticks;
+
+		procedure clock(state : in OutputState_t) is
+		begin
+			sclState <= state;
+			case state is
+				when Low =>
+					counter := low_ticks;
+				when High =>
+					counter := high_ticks;
+				when Released =>
+					counter := high_ticks;
+			end case;
+		end procedure;
+
+		procedure enterStart is
+		begin
+			assert SDA /= '0';
+			assert SCL /= '0';
+			sdaState        <= Low;
+			counter         := start_ticks;
+			controllerState <= Start;
+		end procedure;
+
+		procedure enterSendDeviceAddr is
+		begin
+			assert SCL /= '0' report "SCL is low on enterSendDeviceAddr";
+
+			clock(Low);
+			shiftRegister   <= device_addr & read;
+			shiftCount      <= 8;
+			controllerState <= SendDeviceAddr;
+		end procedure;
+
+		procedure enterSendDeviceAddrDone is
+		begin
+			assert SCL /= '0' report "SCL is low on enterSendDeviceAddrDone";
+
+			clock(Low);
+			sdaState        <= Released;
+			controllerState <= SendDeviceAddrDone;
+			ackState        <= Waiting;
+		end procedure;
+
+		procedure enterWrite is
+		begin
+			assert SCL /= '0' report "SCL is low on enterWrite";
+
+			clock(Low);
+			sdaState        <= Low;
+			shiftRegister   <= dataIn;
+			shiftCount      <= 8;
+			controllerState <= WriteData;
+		end procedure;
+
+		procedure enterRead is
+		begin
+			assert SCL /= '0' report "SCL is low on enterRead";
+
+			clock(Low);
+			sdaState        <= Released;
+			shiftRegister   <= x"00";
+			shiftCount      <= 8;
+			controllerState <= ReadData;
+		end procedure;
+
+		procedure enterWriteDone is
+		begin
+			assert shiftCOunt = 0;
+			assert SCL /= '0' report "SCL is low on enterWriteDone";
+
+			clock(Low);
+			sdaState        <= Released;
+			controllerState <= WriteDone;
+			ackState        <= Waiting;
+		end procedure;
+
+		procedure enterReadDone is
+		begin
+			assert shiftCount = 0;
+			assert SCL /= '0' report "SCL is low on enterReadDone";
+
+			clock(Low);
+			dataOut         <= shiftRegister;
+			controllerState <= ReadDone;
+			ackState        <= Waiting;
+		end procedure;
+
+		procedure enterStop is
+		begin
+			assert SCL /= '0' report "SCL is low on enter stop";
+			
+			clock(Low);
+			sdaState        <= Low;
+			controllerState <= Stop;
+		end procedure;
+
+		procedure writeBit is
+		begin
+			assert sdaState /= Released report "SDA is released during writes.";
+
+			case sclState is
+				when Low =>
+					if shiftRegister(shiftCount - 1) = '1' then
+						sdaState <= High;
+					else
+						sdaState <= Low;
+					end if;
+					if counter = 0 then
+						clock(High);
+					end if;
+				when High =>
+					if counter = 0 then
+						if shiftCount /= 1 then
+							clock(Low);
+						end if;
+						shiftCount <= shiftCount - 1;
+					end if;
+				when Released =>
+					assert False;
+			end case;
+		end procedure writeBit;
+
+		procedure readBit is
+		begin
+			assert sdaState = Released;
+
+			case sclState is
+				when Low =>
+					if counter = 0 then
+						clock(High);
+					end if;
+				when High =>
+					shiftRegister(shiftCount - 1) <= SDA;
+					if counter = 0 then
+						shiftCount <= shiftCount - 1;
+						clock(Low);
+					end if;
+				when Released =>
+					assert False;
+			end case;
+		end procedure;
+
+		procedure sendAck is
+		begin
+			case sclState is
+				when Low =>
+					if en = '0' then
+						sdaState <= High;
+					else
+						sdaState <= Low;
+					end if;
+					if counter = 0 then
+						clock(High);
+					end if;
+				when High =>
+					if counter = 0 then
+						clock(Low);
+						sdaState <= Released;
+					end if;
+				when Released =>
+					assert False;
+			end case;
+		end procedure;
+
+		procedure receiveAck is
+		begin
+			assert sdaState = Released report "SDA is not released on waiting for ack";
+
+			case sclState is
+				when Low =>
+					if counter = 0 then
+						clock(Released);
+					end if;
+				when High =>
+					assert False report "SCL state moved to high on receive ack";
+				when Released =>
+					if SCL = '0' then
+						-- hold the ticks until SCL is released by the slave
+						counter := counter + 1;
+					elsif counter = 0 then
+						if SDA = '0' then
+							ackState <= Ok;
+						else
+							ackState <= Error;
+						end if;
+					end if;
+			end case;
+		end procedure;
+
 	begin
 		if (rst = '1') then
 			controllerState <= Idle;
@@ -91,170 +287,85 @@ begin
 					sclState <= Released;
 					sdaState <= Released;
 					if en = '1' then
-						counter         := start_ticks;
-						controllerState <= Start;
+						enterStart;
 					end if;
 				when Start =>
-					sdaState <= Low;
-					sclState <= High;
 					if counter = 0 then
-						controllerState   <= WriteData;
-						shiftRegister     <= device_addr & read;
-						shiftCount        <= 8;
-						sendingDeviceAddr <= '1';
+						enterSendDeviceAddr;
 					end if;
-				when WriteData =>
-					case sclState is
-						when Low =>
-							if counter = data_setup_ticks then
-								if shiftRegister(shiftCount - 1) = '1' then
-									sdaState <= High;
-								else
-									sdaState <= Low;
-								end if;
+				when SendDeviceAddr =>
+					if shiftCount = 0 then
+						enterSendDeviceAddrDone;
+					else
+						writeBit;
+					end if;
+				when SendDeviceAddrDone =>
+					case ackState is
+						when Waiting =>
+							receiveAck;
+						when Ok =>
+							if en = '0' then
+								enterStop;
+							elsif read = '1' then
+								enterRead;
+							else
+								enterWrite;
 							end if;
-							if counter = 0 then
-								-- let the pullup bring it high so we can handle ack clock stretch
-								sclState <= Released;
-								counter  := high_ticks;
-							end if;
-						when High =>
-							if counter = 0 then
-								sclState <= Low;
-								counter  := low_ticks;
-							end if;
-						when Released =>
-							if shiftCount /= 0 and counter = 0 then
-								-- ugly hack to release SDA at the same time as clock going low.
-								if shiftCount = 1 then
-									sdaState        <= Released;
-									controllerState <= WriteDone;
-								end if;
-								shiftCount <= shiftCount - 1;
-								sclState   <= Low;
-								counter    := low_ticks;
-							end if;
+						when Error =>
+							enterStop;
 					end case;
+				when WriteData =>
+					if shiftCount = 0 then
+						enterWriteDone;
+					else
+						writeBit;
+					end if;
 				when WriteDone =>
-					-- get ack and check for continue
-					case sclState is
-						when Low =>
-							sdaState <= Released;
-							if counter = 0 then
-								sclState <= Released;
+					case ackState is
+						when Waiting =>
+							receiveAck;
+						when Ok =>
+							if en = '0' then
+								enterStop;
+							elsif read = '1' then
+								enterStart;
+							else
+								enterWrite;
 							end if;
-						when High =>
-							null;
-						when Released =>
-							if SCL = '1' or SCL = 'H' then
-								-- setup for 1 high pulse before moving on
-								counter  := high_ticks;
-								sclState <= High;
-								if SDA = '1' or SDA = 'H' then
-									-- Device NACK'd, bail out
-									controllerState <= Stop;
-								else
-									-- keep SDA low so we don't generate a stop condition
-									sdaState <= Low;
-									if en = '0' then
-										controllerState <= Stop;
-									elsif read = '1' then
-										-- check sendingDeviceAddr to figure out if we're in a doing a repeated start
-										if sendingDeviceAddr = '1' then
-											sendingDeviceAddr <= '0';
-											shiftRegister <= x"00";
-											shiftCount <= 8;
-											controllerState <= ReadData;
-										else
-											controllerState <= Start;
-										end if;
-									else
-										-- keep writing so latch the dataIn and keep going
-										sendingDeviceAddr <= '0';
-										shiftRegister <= dataIn;
-										shiftCount    <= 8;
-										controllerState <= WriteData;
-									end if;
-								end if;
-							end if;
+						when Error =>
+							enterStop;
 					end case;
 				when ReadData =>
-					sdaState <= Released;
-					case sclState is
-						when Low =>
-							if counter = 0 then
-								sclState <= Released;
-								counter  := high_ticks;
-								if shiftCount /= 0 then
-									shiftRegister(shiftCount - 1) <= SDA;
-								end if;
-							end if;
-						when High =>
-							if counter = 0 then
-								counter  := low_ticks;
-								sclState <= Low;
-								if shiftCount /= 0 then
-									sdaState <= Released;
-								end if;
-							end if;
-						when Released =>
-							if counter = 0 then
-								if shiftCount = 1 then
-									dataOut         <= shiftRegister;
-									controllerState <= ReadDone;
-									sclState <= Low;
-									counter := low_ticks;
-								else
-									shiftCount <= shiftCount - 1;
-									sclState   <= Low;
-									counter    := low_ticks;
-								end if;
-							end if;
-					end case;
+					if shiftCount = 0 then
+						enterReadDone;
+					else
+						readBit;
+					end if;
 				when ReadDone =>
-					-- do ack and check for continue here
-					case sclState is 
-						when Low =>
-							sdaState <= Low;
-							if counter = 0 then
-								sclState <= High;
-								counter := high_ticks;
-								if en = '0' then
-									-- user says done, so stop the transaction
-									controllerState <= Stop;
-								elsif read = '1' then
-									-- user says more to read, so keep reading
-									shiftRegister <= x"00";
-									shiftCount <= 8;
-									controllerState <= ReadData;
-								else
-									-- user wants to switch mode, do repeated start
-									counter := start_ticks;
-									controllerState <= Start;
-								end if;
+					case ackState is
+						when Waiting =>
+							sendAck;
+						when Ok =>
+							if en = '0' then
+								enterStop;
+							elsif read = '1' then
+								enterRead;
+							else
+								enterStart;
 							end if;
-						when High =>
-							assert False; -- not reached
-						when Released =>
-							if counter = 0 then
-								sclState <= Low;
-								counter := low_ticks;
-							end if;
+						when Error =>
+							assert False;
 					end case;
 				when Stop =>
 					case sclState is
 						when Low =>
-							if counter = data_setup_ticks then
-								sdaState <= Low;
-							elsif counter = 0 then
-								sclState <= Released;
-								counter  := stop_ticks;
+							sdaState <= Low;
+							if counter = 0 then
+								clock(Released);
+								counter := stop_ticks;
 							end if;
 						when High =>
-							if counter = 0 then
-								sclState <= Low;
-								counter  := low_ticks;
-							end if;
+							assert False;
 						when Released =>
 							if counter = 0 then
 								sdaState        <= Released;
--- i2c_controller_tb.vhd
+++ i2c_controller_tb.vhd
@@ -6,69 +6,66 @@ entity i2c_controller_tb is
 end entity i2c_controller_tb;
 
 architecture RTL of i2c_controller_tb is
-	constant clock_period : time := 1 ns;
-	constant device_addr : std_logic_vector(6 downto 0) := "1110010";
-	
+	constant clock_period : time                         := 1 ns;
+	constant device_addr  : std_logic_vector(6 downto 0) := "1110010";
+
 	component i2c_controller
 		generic(device_addr  : std_logic_vector(6 downto 0) := device_addr;
-			    clock_period : time := clock_period;
-			    t_start_hold : time := 4 ns;
-			    t_stop_hold  : time := 4 ns;
-			    t_pulse_low  : time := 5 ns;
-			    t_pulse_high : time := 2 ns;
-			    t_data_setup : time := 1 ns);
-		port(clk      : in    std_logic;
-			 rst      : in    std_logic;
-			 read, en : in    std_logic;
-			 dataIn   : in    std_logic_vector(7 downto 0);
-			 dataOut  : out   std_logic_vector(7 downto 0);
-			 done     : out   std_logic;
-			 busy     : out   std_logic;
-			 currentState : out std_logic_vector(7 downto 0);
-			 SDA, SCL : inout std_logic);
+			    clock_period : time                         := clock_period;
+			    t_start_hold : time                         := 4 ns;
+			    t_stop_hold  : time                         := 4 ns;
+			    t_pulse_low  : time                         := 5 ns;
+			    t_pulse_high : time                         := 2 ns;
+			    t_data_setup : time                         := 1 ns);
+		port(clk          : in    std_logic;
+			 rst          : in    std_logic;
+			 read, en     : in    std_logic;
+			 dataIn       : in    std_logic_vector(7 downto 0);
+			 dataOut      : out   std_logic_vector(7 downto 0);
+			 done         : out   std_logic;
+			 busy         : out   std_logic;
+			 currentState : out   std_logic_vector(7 downto 0);
+			 SDA, SCL     : inout std_logic);
 	end component i2c_controller;
-	
+
 	type tristate is (HIGH, LOW, Z);
-	
+
 	signal clk, rst, read, en : std_logic := '0';
-	signal dataIn, dataOut : std_logic_vector(7 downto 0);
-	signal done, busy : std_logic;
-	signal SDA, SCL : std_logic;
-	
-	signal test_done : std_logic := '0';
-	signal deviceAddr : std_logic_vector(7 downto 0) := x"00";
-	signal data : std_logic_vector(7 downto 0) := x"00";
-	
+	signal dataIn, dataOut    : std_logic_vector(7 downto 0);
+	signal done, busy         : std_logic;
+	signal SDA, SCL           : std_logic;
+
+	signal test_done  : std_logic                    := '0';
+
 	signal currentState : std_logic_vector(7 downto 0);
-	
-	signal sdaBuffer : tristate := Z;
+
+	signal sdaBuffer      : tristate := Z;
 	signal sdaBufferState : integer range 0 to 2;
 begin
-	
-	uut: i2c_controller
-		port map(clk     => clk,
-			     rst     => rst,
-			     read    => read,
-			     en      => en,
-			     dataIn  => dataIn,
-			     dataOut => dataOut,
-			     done    => done,
-			     busy    => busy,
+	uut : i2c_controller
+		port map(clk          => clk,
+			     rst          => rst,
+			     read         => read,
+			     en           => en,
+			     dataIn       => dataIn,
+			     dataOut      => dataOut,
+			     done         => done,
+			     busy         => busy,
 			     currentState => currentState,
-			     SDA     => SDA,
-			     SCL     => SCL);
-	
+			     SDA          => SDA,
+			     SCL          => SCL);
+
 	with sdaBuffer select SDA <=
 		'Z' when Z,
 		'1' when High,
 		'0' when Low;
-	
-	with sdaBuffer select sdaBufferState <= 
+
+	with sdaBuffer select sdaBufferState <=
 		0 when Low,
 		1 when High,
 		2 when Z;
-	
-	clock_driver: process
+
+	clock_driver : process
 	begin
 		if test_done = '1' then
 			wait;
@@ -76,150 +73,170 @@ begin
 		wait for clock_period / 2;
 		clk <= clk xor '1';
 	end process;
-	
-	i2c_device: process
-	begin
-		-- wait for start condition
-		wait until falling_edge(SDA);
-		assert SCL = '1' or SCL = 'H';
-		
-		wait until SCL = '0';
-		
-		for i in 7 downto 0 loop
-			wait until SCL = '1' or SCL = 'H';
-			deviceAddr(i) <= SDA;
-			wait until SCL = '0';
-		end loop;
-		
-		assert deviceAddr = device_addr & '0';
-		
-		sdaBuffer <= Low;
-		wait until SCL = '1' or SCL = 'H';
-		wait until SCL = '0';
-		sdaBuffer <= Z;
-		
-		for i in 7 downto 0 loop
-			wait until SCL = '1' or SCL = 'H';
-			data(i) <= SDA;
-			wait until SCL = '0';
-		end loop;
-		
-		assert data = x"FF";
-		
-		sdaBuffer <= Low;
-		wait until SCL = '1' or SCL = 'H';
-		wait until SCL = '0';
-		sdaBuffer <= Z;
-		
-		for i in 7 downto 0 loop
-			wait until SCL = '1' or SCL = 'H';
-			data(i) <= SDA;
-			wait until SCL = '0';
-		end loop;
-		
-		assert data = x"00";
-		
-		sdaBuffer <= Low;
-		wait until SCL = '1' or SCL = 'H';
-		wait until SCL = '0';
-		sdaBuffer <= Z;
-		
-		wait until rising_edge(SDA);
-		assert SCL = '1' or SCL = 'H';
-		-- write test done stop condition
-		
-		
-		wait until falling_edge(SDA);
-		assert SCL = '1' or SCL = 'H';
-		
-		deviceAddr <= x"00";
-		
-		wait until SCL = '0';
-		
-		for i in 7 downto 0 loop
-			wait until SCL = '1' or SCL = 'H';
-			deviceAddr(i) <= SDA;
-			wait until SCL = '0';
-		end loop;
-		
-		assert deviceAddr = device_addr & '1';
-		
-		sdaBuffer <= Low;
-		wait until SCL = '1' or SCL = 'H';
-		wait until SCL = '0';
-		
-		data <= x"33";
-		
-		for i in 7 downto 0 loop
-			if data(i) = '1' then
-				sdaBuffer <= High;
-			else
-				sdaBuffer <= Low;
-			end if;
-			wait until SCL = '1' or SCL = 'H';
-			wait until SCL = '0';
-		end loop;
+
+	i2c_device : process
+		procedure handleWrite(expected : std_logic_vector(7 downto 0)) is
+			variable actual : std_logic_vector(7 downto 0) := x"00";
+		begin
+			assert SCL = '0';
+			for i in 7 downto 0 loop
+				wait until SCL = '1' or SCL = 'H';
+				actual(i) := SDA;
+				wait until SCL = '0';
+			end loop;
+
+			assert actual = expected report "Incorrect write byte";
 			
-		sdaBuffer <= Z;
-		wait until SCL = '1' or SCL = 'H';
-		assert SDA = '0';
-		wait until SCL = '0';
-		
-		data <= x"11";
-		
-		for i in 7 downto 0 loop
-			if data(i) = '1' then
-				sdaBuffer <= High;
-			else
-				sdaBuffer <= Low;
-			end if;
+			sdaBuffer <= Low;
 			wait until SCL = '1' or SCL = 'H';
 			wait until SCL = '0';
-		end loop;
+			sdaBuffer <= Z;
+		end procedure;
 		
-		sdaBuffer <= Z;
-		wait until SCL = '1' or SCL = 'H';
-		assert SDA = '0';
-		wait until SCL = '0';
+		procedure waitForStart is
+		begin
+			wait until falling_edge(SDA);
+			assert SCL /= '0' report "SCL is low during start condition.";
+		end procedure;
 		
-		wait until rising_edge(SDA);
-		assert SCL = '1' or SCL = 'H';
+		procedure waitForStop is
+		begin
+			wait until rising_edge(SDA);
+			assert SCL /= '0' report "SCL is low on stop condition";
+		end procedure;
 		
+		procedure write_test is
+		begin
+			waitForStart;
+
+			wait until SCL = '0';
+
+			handleWrite(device_addr & read);
+
+			handleWrite(x"FF");
+
+			handleWrite(x"00");
+
+			waitForStop;
+		end procedure;
+		
+		procedure handleRead(value : std_logic_vector(7 downto 0);
+			                 ack : std_logic) is
+		begin
+			assert SCL = '0';
+			for i in 7 downto 0 loop
+				if value(i) = '1' then
+					sdaBuffer <= High;
+				else
+					sdaBuffer <= Low;
+				end if;
+				wait until SCL = '1' or SCL = 'H';
+				wait until SCL = '0';
+			end loop;
+
+			sdaBuffer <= Z;
+			wait until SCL = '1' or SCL = 'H';
+			assert SDA = ack;
+			wait until SCL = '0';
+		end procedure;
+		
+		procedure read_test is
+		begin
+			waitForStart;
+
+			wait until SCL = '0';
+
+			handleWrite(device_addr & '1');
+
+			handleRead(x"33", '0');
+
+			handleRead(x"11", '0');
+
+			waitForStop;
+		end procedure;
+	
+		procedure write_read_test is
+		begin
+			waitForStart;
+			wait until SCL = '0';
+			
+			handleWrite(device_addr & '0');
+			
+			handleWrite(x"A1");
+			
+			waitForStart;
+			
+			wait until SCL = '0';
+			handleWrite(device_addr & '1');
+			handleRead(x"B1", '0');
+			
+		end procedure;
+	begin
+		
+		write_test;
+		
+		--read_test;
+
+--		write_read_test;
 		wait;
 	end process;
+
+	test_user : process
+		procedure write_test is
+		begin
+			en     <= '1';
+			read   <= '0';
+			dataIn <= x"FF";
+			wait until done = '1';
+
+			dataIn <= x"00";
+
+			wait until done = '0';
+			wait until done = '1';
+			en <= '0';
+
+			wait until busy = '0';
+		end procedure;
+
+		procedure read_test is
+		begin
+			en   <= '1';
+			read <= '1';
+			wait until busy = '1';
+
+			wait until done = '1';
+			assert dataOut = x"33";
+			wait until done = '0';
+
+			wait until done = '1';
+			assert dataOut = x"11";
+			en <= '0';
+			wait until busy = '0';
+
+		end procedure;
 	
-	test_user: process
+		procedure write_read_test is
+		begin
+			en <= '1';
+			read <= '0';
+			dataIn <= x"A1";
+			wait until done = '1';
+			
+			read <= '1';
+			wait until done = '1';
+			assert dataOut = x"B1";
+			
+		end procedure;
 	begin
-		en <= '1';
-		read <= '0';
-		dataIn <= x"FF";
-		wait until done = '1';
+		write_test;
 
-		dataIn <= x"00";
-
-		wait until done = '0';
-		wait until done = '1';
-		en <= '0';
+		--read_test;
 		
-		wait until busy = '0';
-		
-		en <= '1';
-		read <= '1';
-		wait until busy = '1';
-		
-		wait until done = '1';
-		assert dataOut = x"33";
-		wait until done = '0';
-		
-		wait until done = '1';
-		assert dataOut = x"11";
-		en <= '0';
-		wait until busy = '0';
+--		write_read_test;
 
 		-- done
 		test_done <= '1';
 		wait;
 	end process;
-		
-	
+
 end architecture RTL;
